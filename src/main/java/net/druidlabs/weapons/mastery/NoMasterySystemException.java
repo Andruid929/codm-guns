507b5b00a198b40e839e10c26ea7a284f97f5ad0
Andruid929 <princeajsa@gmail.com>
2025-05-20 17:04:33 +0200

    Added documentation for mastery exception classes

--- src/main/java/net/druidlabs/weapons/mastery/NoMasterySystemException.java
+++ src/main/java/net/druidlabs/weapons/mastery/NoMasterySystemException.java
@@ -1,5 +1,14 @@
 package net.druidlabs.weapons.mastery;
 
+/**
+ * Thrown when there is an attempt to get mastery names for
+ * secondary weapons which do not support the mastery system.
+ *
+ * @since 1.0
+ * @author Andrew Jones
+ * @see java.lang.UnsupportedOperationException
+ * */
+
 public class NoMasterySystemException extends UnsupportedOperationException {
 
     public NoMasterySystemException() {
--- src/main/java/net/druidlabs/weapons/mastery/NotMasteredException.java
+++ src/main/java/net/druidlabs/weapons/mastery/NotMasteredException.java
@@ -2,6 +2,13 @@ package net.druidlabs.weapons.mastery;
 
 import net.druidlabs.weapons.Weapon;
 
+/**
+ * Thrown when a weapon's mastery name is blank or equals {@code MasteryCollection.NOT_MASTERED}.
+ *
+ * @author Andrew Jones
+ * @since 2.0
+ */
+
 public class NotMasteredException extends NullPointerException {
 
     private final Weapon weapon;
